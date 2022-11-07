3161feeb1a5a24ce07ca3e6f9d81e783b6a19936
DrKelvin <kelvinronald12@gmail.com>
2022-11-07 16:06:03 -0300

    adicionando a estrutura de pastas que será usada no projeto e começando a primeira página de registro

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ImgMainDesktopWoman from '../assets/images/DrawKit-Vector-Illustration-ecommerce-02-transparent.png';
+
+export default function(){
+  return(
+    <div className="main">
+      <h1></h1>
+      <img src={ImgMainDesktopWoman} alt="" />
+    </div>
+  );
+}

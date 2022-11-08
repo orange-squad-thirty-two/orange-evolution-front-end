822662859e7663b24e5b49baffdc382f364ee51d
eemr3 <eemr3@yahoo.com.br>
2022-11-08 17:50:05 -0300

    feat: :fire: routes/Routes.jsx
    adiciona rota para a página de home

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,4 +1,5 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
+import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register/Index';
 
@@ -8,6 +9,7 @@ export function Routes() {
       <Route path="/login" component={Login} />
       <Redirect exact from="/" to="/login" />
       <Route path="/register" component={Register} />
+      <Route path="/home" component={Home} />
     </Switch>
   );
 }

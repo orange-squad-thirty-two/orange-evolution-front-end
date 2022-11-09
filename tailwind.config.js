d2729e5e6960c0b147316d43e560c6c5962b1113
eemr3 <eemr3@yahoo.com.br>
2022-11-09 15:09:34 -0300

    style: :art: Login/index.jsx
    adiciona responsividade a tela de login

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
       lg: ['20px', '28px'],
       xl: ['36px', '36px'],
     },
+    backgroundImage: {
+      'login-not': 'none',
+      'login-gradient':
+        'linear-gradient(324.33deg, #FF7823 1.18%, rgba(255, 120, 35, 0) 50.24%)',
+    },
     extend: {},
   },
   plugins: [],

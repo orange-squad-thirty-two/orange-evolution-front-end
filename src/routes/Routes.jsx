e1233563a3869bc0084f293741a2e8a108f6c171
eemr3 <eemr3@yahoo.com.br>
2022-11-10 17:24:48 -0300

    feat: :fire: GlobalProvider.jsx
    cria contexto global para a aplicação

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,4 +1,5 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import { GlobalProvider } from '../context/GlobalProvider';
 import HomeProvider from '../context/HomeProvider';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
@@ -7,12 +8,14 @@ import Register from '../pages/Register/Index';
 export function Routes() {
   return (
     <Switch>
-      <Route path="/login" component={Login} />
-      <Redirect exact from="/" to="/login" />
-      <Route path="/register" component={Register} />
-      <HomeProvider>
-        <Route path="/home" component={Home} />
-      </HomeProvider>
+      <GlobalProvider>
+        <Route exact path="/" component={Login} />
+
+        <Route path="/register" component={Register} />
+        <HomeProvider>
+          <Route path="/home" component={Home} />
+        </HomeProvider>
+      </GlobalProvider>
     </Switch>
   );
 }

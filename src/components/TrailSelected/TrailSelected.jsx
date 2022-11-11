8b90efd2eb8451e1f5b252cc280d7e3bc846f593
Rafael Souza <rafael93.souzasfc5@gmail.com>
2022-11-11 15:03:05 -0300

    Adicionado a requisição das trilhas do back-end

--- src/components/TrailSelected/TrailSelected.jsx
+++ src/components/TrailSelected/TrailSelected.jsx
@@ -64,8 +64,17 @@ function TrailSelected({ trails }) {
                 </div>
             }
             <h1 className="title-trial">{name}</h1>
-            <h4 className="sub-title-trial">{subTitle}</h4>
-            <p className="description-trial">{description}</p>
+            <h4 className="sub-title-trial">{subTitle
+                ? subTitle
+                : `Se você chegou até aqui, é porque quer aprender mais sobre tecnologia, especialmente sobre ${name}`
+
+            }</h4>
+            <p className="description-trial">
+                {description
+                    ? description
+                    : ` O Orange Evolution consiste em trilhas totalmente gratuitas para que você possa iniciar a sua carreira na tecnologia. Você terá acesso a vídeos, lives, artigos, apostilas e até cursos gratuitos, além desses conteúdos serem da Orange Juice, de parceiros e empresas que confiamos. 
+        
+        Essa trilha foi montada pensando em quem está começando na área, ou passando por uma migração de carreira e ainda não sabe exatamente o que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!`}</p>
 
 
             {trail &&
--- src/pages/Trails/Trails.jsx
+++ src/pages/Trails/Trails.jsx
@@ -1,19 +1,29 @@
+import { useEffect, useState } from "react";
 import Aside from "../../components/Aside/Aside"
 import Header from "../../components/Header/Header";
 import TrailSelected from "../../components/TrailSelected/TrailSelected";
+import { useGlobalContext } from "../../context/GlobalProvider";
 import trails from "../../database/trails"
 import './style.css';
 
 function Trails() {
+    const { dataTrails } = useGlobalContext()
+    const [trailsAll, setTrailAll] = useState(trails);
+
+    useEffect(() => {
+        if (dataTrails.data) {
+            setTrailAll(dataTrails.data)
+        }
+    }, [setTrailAll, dataTrails])
     return (
         <>
-            <Header trails={trails} />
+            <Header trails={trailsAll} />
             <section className="container-trails relative flex">
                 <div className="z-20">
-                    <Aside trails={trails} />
+                    <Aside trails={trailsAll} />
                 </div>
                 <div className="div-trail">
-                    <TrailSelected trails={trails} />
+                    <TrailSelected trails={trailsAll} />
                 </div>
             </section>
         </>

ea8249909bd5dede1284318c5215daf98c5dfdfb
Rafael Souza <rafael93.souzasfc5@gmail.com>
2022-11-13 18:36:17 -0300

    Adicionado Responsividade a página de trilhas

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,39 +1,87 @@
-import { NavLink } from "react-router-dom";
+import { useEffect } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import iconCloseMenu from '../../assets/icons/icon-close-card.svg';
 import './style.css';
 
-function Aside({ trails }) {
-
+function Aside({ trails, showMenu, setShowMenu }) {
+    const history = useHistory();
     function handleActivePath(path) {
         if (window.location.pathname === path) {
             return true;
         }
         return false;
     }
+    useEffect(() => {
+        document.querySelector("body").style.overflow = showMenu ? 'hidden' : 'initial'
+    }, [showMenu]);
+
+    function handleNavigate() {
+        history.replace("/home")
+        setShowMenu(false)
+    }
     return (
-        <aside>
-            <div className='w-28 pl-4'>
-                <button className="button-trails">
-                    Trilhas
-                </button>
-                {trails.map(trail => {
-                    return (
-                        <NavLink
-                            key={trail.id}
-                            to={`/trails/${trail.id}`}
-                        >
-                            <button
-                                className={
-                                    handleActivePath(`/trails/${trail.id}`)
-                                        ? "pl-3 is-active"
-                                        : "pl-3 inactive"
-                                }
+        <aside className={!showMenu ? "aside" : "aside-mobile"}>
+            {showMenu &&
+                <img
+                    className="icon-close-modal-menu"
+                    onClick={() => setShowMenu(false)}
+                    src={iconCloseMenu} alt="Clique para fechar o menu de navegação" />
+
+            }
+
+            {!showMenu ?
+                <div className='div-nav-desktop w-28 pl-4'>
+                    <button className="button-trails">
+                        Trilhas
+                    </button>
+                    {trails.map(trail => {
+                        return (
+                            <NavLink
+                                key={trail.id}
+                                to={`/trails/${trail.id}`}
                             >
-                                {trail.nome.split(" ")[0]}
-                            </button>
-                        </NavLink>
-                    )
-                })}
-            </div>
+                                <button
+                                    className={
+                                        handleActivePath(`/trails/${trail.id}`)
+                                            ? "pl-3 is-active"
+                                            : "pl-3 inactive"
+                                    }
+                                >
+                                    {trail.nome.split(" ")[0]}
+                                </button>
+                            </NavLink>
+                        )
+                    })}
+                </div>
+                :
+                <div className='div-nav-desktop w-28 pl-4'>
+                    <button className="button-trails-mobile"
+                        onClick={() => handleNavigate()}
+                    >
+                        Home
+
+                    </button>
+                    {trails.map(trail => {
+                        return (
+                            <NavLink
+                                key={trail.id}
+                                to={`/trails/${trail.id}`}
+                            >
+                                <button
+                                    onClick={() => setShowMenu(false)}
+                                    className={
+                                        handleActivePath(`/trails/${trail.id}`)
+                                            ? "pl-3 is-active-mobile"
+                                            : "pl-3 inactive-mobile"
+                                    }
+                                >
+                                    {trail.nome.split(" ")[0]}
+                                </button>
+                            </NavLink>
+                        )
+                    })}
+                </div>
+            }
         </aside>
     )
 }

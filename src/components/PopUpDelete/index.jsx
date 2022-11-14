bb8cb3978fc281adf5fd8e53041c725bd48f34d7
Rafael Souza <rafael93.souzasfc5@gmail.com>
2022-11-14 15:09:47 -0300

    Adicionado Pop-up de deletrar curso

---
+++ src/components/PopUpDelete/index.jsx
@@ -0,0 +1,43 @@
+import JsCookie from 'js-cookie';
+import iconBalonSet from '../../assets/icons/icon-balon-set.svg';
+import { api } from '../../services/api';
+import "./style.css";
+
+function PopUpDelete({ setPopupDelet, id, trial }) {
+
+    async function handleDelete(id) {
+        const token = JsCookie.get('token');
+        try {
+            const response = await api.delete(`/classes/${Number(trial) / Number(id)}`,
+                { headers: { Authorization: `Bearer ${token}` } });
+            console.log(response)
+            setPopupDelet(false);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <div className='div-delete-classes'>
+            <img
+                className='img-set-balon'
+                src={iconBalonSet} alt="icone de seta" />
+            <span>Apagar Aula?</span>
+            <div className='btns-yes-no'>
+                <button
+                    className='btn-yes'
+                    onClick={() => handleDelete(id)}
+                >
+                    Sim
+                </button>
+                <button
+                    className='btn-no'
+                    onClick={() => setPopupDelet(false)}
+                >
+                    Não
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default PopUpDelete;
